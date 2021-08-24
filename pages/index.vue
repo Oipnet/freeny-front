5a1cf5a2e3cb2193d46844e25ced053b987f8349
Arnaud POINTET <arnaud.pointet@gmail.com>
2021-08-24 18:50:06 +0200

    Content homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,38 @@
 <template>
-  <div class="container_comming-soon">
-    <img src="~/assets/images/logo/logo_small_icon_only.png" alt="Logo FREE aNd You">
-    <h1>FREE And You</h1>
-    <p>Le site est en cours de développement et sera bientot disponible</p>
+  <div>
+    <header>
+      <h1>FREE aNd You</h1>
+      <h2>Libre et vous ?</h2>
+      <p>Ici l'honetté est notre devise</p>
+      Pour en savoir plus <a href="#">Contacter notre équipe</a>
+    </header>
+    <section class="honetete_profil">
+      <h1>Honeteté sur les profils</h1>
+      <p>
+        Chez Freeny, les faux profils sont notre hantise.<br>
+        Freeny travaille pour vous proposer un site ou la création de faux compte sera complexe.<br>
+        Freeny vous promet de ne pas avoir recours à l'utilisation de robots ou de fake mails.<br>
+        Freeny s'engage a prendre en compte tous signalement de votre part.
+      </p>
+    </section>
+    <section class="honetete_prix">
+      <h1>Honeteté sur les prix</h1>
+      <p>
+        Chez Frenny nous somme d'ancien utilisateur de site de rencontre et connaissons leur fonctionnement.<br>
+        Freeny ne sera pas une nouvelle plateforme avec un nouvel abonnement exorbitant à souscrire.<br>
+        Freeny sera financé grâce a l'affichage de publicité.<br>
+        Freeny vous proposera des options de confort a prix raisonable.<br>
+      </p>
+    </section>
+    <section class="discretion">
+      <h1>Discretion et respect de votre vie privé</h1>
+      <p>
+        Chez Freeny nous savons que vous aimez ne pas être derangé.<br>
+        Freeny ne vous demandera pas de carte bleu et n'effectura aucune transaction sur votre compte bancaire.<br>
+        Freeny utilisera des systèmes de micro paiement pour toutes soucriptions à une option.<br>
+        Freeny ne vous enverra aucun mail sur votre boite personnelle, toutes communication officielle se feront via nos reseaux sociaux.<br>
+      </p>
+    </section>
   </div>
 </template>
 
