5292d616eca416e2dc00d30e485738fbd2df62cb
Arnaud POINTET <arnaud.pointet@gmail.com>
2021-08-24 19:25:42 +0200

    Typo FreeNy

--- pages/index.vue
+++ pages/index.vue
@@ -14,28 +14,28 @@
     <section class="honetete_profil">
       <h1>Honeteté sur les profils</h1>
       <p>
-        Chez Freeny, les <strong>faux profils</strong> sont notre hantise.<br>
-        Freeny travaille pour vous proposer un site ou la <strong>création de faux compte sera complexe</strong>.<br>
-        Freeny vous promet de ne pas avoir recours à l'<strong>utilisation de robots ou de fake mails</strong>.<br>
-        Freeny s'engage a <strong>prendre en compte tous signalement</strong> de votre part.
+        Chez FreeNy, les <strong>faux profils</strong> sont notre hantise.<br>
+        FreeNy travaille pour vous proposer un site ou la <strong>création de faux compte sera complexe</strong>.<br>
+        FreeNy vous promet de ne pas avoir recours à l'<strong>utilisation de robots ou de fake mails</strong>.<br>
+        FreeNy s'engage a <strong>prendre en compte tous signalement</strong> de votre part.
       </p>
     </section>
     <section class="honetete_prix">
       <h1>Honeteté sur les prix</h1>
       <p>
         Chez Frenny nous somme d'<strong>anciens utilisateurs de site de rencontre</strong> et connaissons leur fonctionnement.<br>
-        Freeny ne sera <strong>pas une nouvelle plateforme avec un nouvel abonnement</strong> exorbitant à souscrire.<br>
-        Freeny sera <strong>financé grâce a l'affichage de publicité</strong>.<br>
-        Freeny vous proposera des <strong>options de confort a prix raisonable</strong>.<br>
+        FreeNy ne sera <strong>pas une nouvelle plateforme avec un nouvel abonnement</strong> exorbitant à souscrire.<br>
+        FreeNy sera <strong>financé grâce a l'affichage de publicité</strong>.<br>
+        FreeNy vous proposera des <strong>options de confort a prix raisonable</strong>.<br>
       </p>
     </section>
     <section class="discretion">
       <h1>Discretion et respect de votre vie privé</h1>
       <p>
-        Chez Freeny nous savons que vous aimez <strong>ne pas être derangé</strong>.<br>
-        Freeny ne vous demandera <strong>pas de carte bleu</strong> et n'effectura <strong>aucune transaction sur votre compte bancaire</strong>.<br>
-        Freeny utilisera des systèmes de <strong>micro paiement</strong> pour toutes soucriptions à une option.<br>
-        Freeny ne vous enverra <strong>aucun mail sur votre boite personnelle</strong>, toutes communication officielle se feront <strong>via nos reseaux sociaux</strong>.<br>
+        Chez FreeNy nous savons que vous aimez <strong>ne pas être derangé</strong>.<br>
+        FreeNy ne vous demandera <strong>pas de carte bleu</strong> et n'effectura <strong>aucune transaction sur votre compte bancaire</strong>.<br>
+        FreeNy utilisera des systèmes de <strong>micro paiement</strong> pour toutes soucriptions à une option.<br>
+        FreeNy ne vous enverra <strong>aucun mail sur votre boite personnelle</strong>, toutes communication officielle se feront <strong>via nos reseaux sociaux</strong>.<br>
       </p>
     </section>
   </div>
