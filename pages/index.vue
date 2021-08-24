eabe2ea07313d380480ac2a0ea04e42b7376ce37
Arnaud POINTET <arnaud.pointet@gmail.com>
2021-08-24 17:26:51 +0200

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container_comming-soon">
+    <img src="~/assets/images/logo/logo_small_icon_only.png" alt="Logo FREE aNd You">
+    <h1>FREE And You</h1>
+    <p>Le site est en cours de développement et sera bientot disponible</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
