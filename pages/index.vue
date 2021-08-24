bd442648e99b0e55c9a038d2a2456d6d772007fe
Arnaud POINTET <arnaud.pointet@gmail.com>
2021-08-24 19:23:33 +0200

    first design homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,41 @@
 <template>
   <div>
-    <header>
-      <h1>FREE aNd You</h1>
-      <h2>Libre et vous ?</h2>
-      <p>Ici l'honetté est notre devise</p>
-      Pour en savoir plus <a href="#">Contacter notre équipe</a>
+    <header class="jumbo">
+      <div>
+      <h1 class="jumbo__title">FREE aNd You</h1>
+      <h2 class="jumbo__subtitle">Libre et vous ?</h2>
+      <p class="jumbo__description">Ici l'honetté est notre devise</p>
+      <div class="jumbo__cta">
+        Pour en savoir plus <a href="#">Contacter notre équipe</a>
+      </div>
+      </div>
+      <img src="@/assets/images/logo/logo_large.png" alt="logo FREE aNd You" width="200px"/>
     </header>
     <section class="honetete_profil">
       <h1>Honeteté sur les profils</h1>
       <p>
-        Chez Freeny, les faux profils sont notre hantise.<br>
-        Freeny travaille pour vous proposer un site ou la création de faux compte sera complexe.<br>
-        Freeny vous promet de ne pas avoir recours à l'utilisation de robots ou de fake mails.<br>
-        Freeny s'engage a prendre en compte tous signalement de votre part.
+        Chez Freeny, les <strong>faux profils</strong> sont notre hantise.<br>
+        Freeny travaille pour vous proposer un site ou la <strong>création de faux compte sera complexe</strong>.<br>
+        Freeny vous promet de ne pas avoir recours à l'<strong>utilisation de robots ou de fake mails</strong>.<br>
+        Freeny s'engage a <strong>prendre en compte tous signalement</strong> de votre part.
       </p>
     </section>
     <section class="honetete_prix">
       <h1>Honeteté sur les prix</h1>
       <p>
-        Chez Frenny nous somme d'ancien utilisateur de site de rencontre et connaissons leur fonctionnement.<br>
-        Freeny ne sera pas une nouvelle plateforme avec un nouvel abonnement exorbitant à souscrire.<br>
-        Freeny sera financé grâce a l'affichage de publicité.<br>
-        Freeny vous proposera des options de confort a prix raisonable.<br>
+        Chez Frenny nous somme d'<strong>anciens utilisateurs de site de rencontre</strong> et connaissons leur fonctionnement.<br>
+        Freeny ne sera <strong>pas une nouvelle plateforme avec un nouvel abonnement</strong> exorbitant à souscrire.<br>
+        Freeny sera <strong>financé grâce a l'affichage de publicité</strong>.<br>
+        Freeny vous proposera des <strong>options de confort a prix raisonable</strong>.<br>
       </p>
     </section>
     <section class="discretion">
       <h1>Discretion et respect de votre vie privé</h1>
       <p>
-        Chez Freeny nous savons que vous aimez ne pas être derangé.<br>
-        Freeny ne vous demandera pas de carte bleu et n'effectura aucune transaction sur votre compte bancaire.<br>
-        Freeny utilisera des systèmes de micro paiement pour toutes soucriptions à une option.<br>
-        Freeny ne vous enverra aucun mail sur votre boite personnelle, toutes communication officielle se feront via nos reseaux sociaux.<br>
+        Chez Freeny nous savons que vous aimez <strong>ne pas être derangé</strong>.<br>
+        Freeny ne vous demandera <strong>pas de carte bleu</strong> et n'effectura <strong>aucune transaction sur votre compte bancaire</strong>.<br>
+        Freeny utilisera des systèmes de <strong>micro paiement</strong> pour toutes soucriptions à une option.<br>
+        Freeny ne vous enverra <strong>aucun mail sur votre boite personnelle</strong>, toutes communication officielle se feront <strong>via nos reseaux sociaux</strong>.<br>
       </p>
     </section>
   </div>
