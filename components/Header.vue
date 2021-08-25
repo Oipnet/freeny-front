812f07411e909daa5b4f15f28101b8d4c5ceda9d
Arnaud POINTET <arnaud.pointet@gmail.com>
2021-08-25 18:27:52 +0200

    Initialise landing page

---
+++ components/Header.vue
@@ -0,0 +1,53 @@
+<template>
+  <header>
+    <img src="@/assets/images/logo/logo_small_icon_only.png" alt="Logo FreeNy">
+    <div class="content">
+      <h1>FREE aNd You</h1>
+      <h2>Libre et vous</h2>
+      <div class="cta">
+        <a href="#">Déjà inscrit</a>
+        <a href="#">Inscription</a>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss" scoped>
+  @import "@/assets/css/_variables";
+
+  header {
+    color: $color-orange;
+    border: 1px solid $color-orange;
+    border-radius: 50%;
+    width: 625px;
+    height: 625px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    margin: auto;
+  }
+
+  h1 {
+    font-size: 4.1em;
+    margin-bottom: 0.5em;
+  }
+
+  h2 {
+    font-size: 1.5em;
+    margin-bottom: 2.25em;
+  }
+
+  .cta {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .content {
+    transform: translateY(-50px);
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,47 +1,14 @@
 <template>
-  <div>
-    <header class="jumbo">
-      <div>
-      <h1 class="jumbo__title">FREE aNd You</h1>
-      <h2 class="jumbo__subtitle">Libre et vous ?</h2>
-      <p class="jumbo__description">Ici l'honetté est notre devise</p>
-      <div class="jumbo__cta">
-        Pour en savoir plus <a href="#">Contacter notre équipe</a>
-      </div>
-      </div>
-      <img src="@/assets/images/logo/logo_large.png" alt="logo FREE aNd You" width="200px"/>
-    </header>
-    <section class="honetete_profil">
-      <h1>Honeteté sur les profils</h1>
-      <p>
-        Chez FreeNy, les <strong>faux profils</strong> sont notre hantise.<br>
-        FreeNy travaille pour vous proposer un site ou la <strong>création de faux compte sera complexe</strong>.<br>
-        FreeNy vous promet de ne pas avoir recours à l'<strong>utilisation de robots ou de fake mails</strong>.<br>
-        FreeNy s'engage a <strong>prendre en compte tous signalement</strong> de votre part.
-      </p>
-    </section>
-    <section class="honetete_prix">
-      <h1>Honeteté sur les prix</h1>
-      <p>
-        Chez Frenny nous somme d'<strong>anciens utilisateurs de site de rencontre</strong> et connaissons leur fonctionnement.<br>
-        FreeNy ne sera <strong>pas une nouvelle plateforme avec un nouvel abonnement</strong> exorbitant à souscrire.<br>
-        FreeNy sera <strong>financé grâce a l'affichage de publicité</strong>.<br>
-        FreeNy vous proposera des <strong>options de confort a prix raisonable</strong>.<br>
-      </p>
-    </section>
-    <section class="discretion">
-      <h1>Discretion et respect de votre vie privé</h1>
-      <p>
-        Chez FreeNy nous savons que vous aimez <strong>ne pas être derangé</strong>.<br>
-        FreeNy ne vous demandera <strong>pas de carte bleu</strong> et n'effectura <strong>aucune transaction sur votre compte bancaire</strong>.<br>
-        FreeNy utilisera des systèmes de <strong>micro paiement</strong> pour toutes soucriptions à une option.<br>
-        FreeNy ne vous enverra <strong>aucun mail sur votre boite personnelle</strong>, toutes communication officielle se feront <strong>via nos reseaux sociaux</strong>.<br>
-      </p>
-    </section>
+  <div class="container_landing">
+    <div>
+      Le descriptif
+    </div>
+    <Header />
+    <div style="text-align:right">
+      TODO : Le menu
+    </div>
   </div>
 </template>
-<script lang="ts">
-import Vue from 'vue'
 
-export default Vue.extend({})
+<script lang="ts" setup>
 </script>
