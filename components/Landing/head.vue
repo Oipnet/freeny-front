312c3392ae3bbb327e7494f89ed31d150831181b
GeoffreyFRANZ <franz.geoffrey@hotmail.fr>
2021-08-27 10:07:43 +0200

    Homepage with header

---
+++ components/Landing/head.vue
@@ -0,0 +1,64 @@
+<template>
+  <header>
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+    <img src="@/assets/images/logo/logo_large.png" class="logo" alt="logo Freeny">
+    <div>
+      <a href="#">
+        <i class="ri-group-line"></i>
+        <span>Visites</span>
+      </a>
+      <a href="#">
+      <i class="ri-thumb-up-line"></i>
+        <span>likes</span>
+      </a>
+      <a href="#">
+      <i class="ri-thumb-up-line"></i>
+        <span>Match</span>
+      </a>
+      <a href="#">
+        <i class="ri-ink-bottle-line"></i>
+        <span>Messages</span>
+      </a>
+      <img src="https://via.placeholder.com/150" class="profile" alt="photo de profile">
+    </div>
+  </header>
+</template>
+
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/css/_variables";
+
+header{
+  display: flex;
+  justify-content: space-between;
+  color: $color-orange;
+  padding: 2rem;
+}
+
+img{
+  width: 15%;
+}
+header div{
+  display: flex;
+  justify-content: end;
+}
+header div a {
+  display: flex;
+  flex-direction: column;
+  margin: 0 0.5rem;
+  align-items: center;
+  text-decoration: none;
+}
+header div a span{
+  text-decoration: underline;
+}
+.profile{
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+
+}
+</style>
---
+++ pages/Homepage.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <LandingHead />
+    <LandingSearch/>
+  </div>
+</template>
+
+<script lang="ts" setup>
+</script>
